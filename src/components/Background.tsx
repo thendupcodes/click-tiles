34faf49a7c1e44932c65a82f441e4e1a05bb3715
Thendup Tsering <thendupcodes@gmail.com>
2024-05-02 21:54:27 -0400

    Fix refs issue

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,27 +1,41 @@
-import { useRef } from 'react';
+import { createRef, useRef } from 'react';
 
 import imageUrls from '@/constants/images.json';
 
 type TilesBackgroundProps = {
   imageIdx: number;
-  imagePosition: number;
+  imagePosition: { x: number, y: number };
   backgroundToggled: boolean;
 }
+
+const useRefsFromArray = <T extends string>(array: T[]) => {
+  // Create a single useRef to hold the array of refs
+  const refs = useRef<(React.MutableRefObject<HTMLDivElement | null>)[]>([]);
+
+  // Populate the array of refs using createRef
+  if (refs.current.length !== array.length) {
+    refs.current = Array.from({ length: array.length }, () => createRef<HTMLDivElement | null>());
+  }
+
+  return refs;
+};
 
 export default function TilesBackground ({
   imageIdx,
   imagePosition,
   backgroundToggled
 }: TilesBackgroundProps) {
+  const imageRefs = useRefsFromArray(imageUrls);
+  
   return (
     <div className="Tiles__background" style={{
       opacity: backgroundToggled ? 1 : 0
     }}>
       {imageUrls.map((url, idx) => {
-        const imageRef = useRef(null);
+        const imageRef = imageRefs?.current[idx];
 
-        if (imageRef?.current != null) {
-          imageRef.current.animate({
+        if (imageRef != null) {
+          imageRef.current?.animate({
             backgroundPosition: `${imagePosition.x}% ${imagePosition.y}%`,
           }, { duration: 5000, fill: 'forwards' });
         }
