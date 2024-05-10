7485dd62559b0e22fd48f36b9ae56fd17d64fd50
Thendup Tsering <thendupcodes@gmail.com>
2024-05-10 15:02:53 -0400

    Style controls and add show controls button

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,3 +1,5 @@
+import Tooltip from "@/components/Tooltip";
+
 type TileControlsProps = {
   tilesToggled: boolean;
   hideControls: boolean;
@@ -20,68 +22,98 @@ export default function TileControls ({
   toggleFullScreen,
 }: TileControlsProps) {
   return (
-    <div className={`Tiles__footer ${tilesToggled && !hideControls ? 'Tiles__footer--show' : ''} ${slowLoad ? 'Tiles__footer--slow-load' : ''}`}>
-      <div className="Tiles__footer-controls">
-        <div
-          tabIndex={0}
-          role="button"
-          className="Key Tiles__key-sc Tiles__key-sc--previous"
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' || e.key === ' ') handleQuotePrevious()
-          }}
-          onClick={() => handleQuotePrevious()}
-        >
-          <div className="Key__icon Tiles__key-sc-icon"><i className="fa-solid fa-caret-left" /></div>
-        </div>
-
-        <div
-          tabIndex={0}
-          role="button"
-          className="Key Tiles__key-sc Tiles__key-sc--next"
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' || e.key === ' ') handleQuoteNext()
-          }}
-          onClick={() => handleQuoteNext()}
-        >
-          <div className="Key__icon Tiles__key-sc-icon"><i className="fa-solid fa-caret-right" /></div>
-        </div>
-
-        <div
-          tabIndex={0}
-          role="button"
-          className="Key Tiles__key-sc Tiles__key-sc--background"
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' || e.key === ' ') toggleBackground()
-          }}
-          onClick={() => toggleBackground()}
-        >
-          <div className="Key__icon Tiles__key-sc-icon"><i className="fa-solid fa-images"></i></div>
-        </div>
-
-        <div
-          tabIndex={0}
-          role="button"
-          className="Key Tiles__key-sc Tiles__key-sc--fullscreen"
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' || e.key === ' ') toggleFullScreen()
-          }}
-          onClick={() => toggleFullScreen()}
-        >
-          <div className="Key__icon Tiles__key-sc-icon"><i className="fa-solid fa-expand"></i></div>
-        </div>
-
-        <div
-          tabIndex={0}
-          role="button"
-          className="Key Tiles__key-sc Tiles__key-sc--hide"
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' || e.key === ' ') toggleControls()
-          }}
-          onClick={() => toggleControls()}
-        >
-          <div className="Key__icon Tiles__key-sc-icon"><i className="fa-solid fa-eye-slash"></i></div>
+    <>
+      <div className={`Tiles__footer ${tilesToggled && hideControls ? 'Tiles__footer--show' : ''}`}>
+        <div className="Tiles__footer-controls">
+          <Tooltip direction="top" delay={0} content="Show controls">
+            <div
+              tabIndex={0}
+              role="button"
+              className="Key Key--show Tiles__key-sc Tiles__key-sc--show"
+              onKeyDown={(e) => {
+                if (e.key === 'Enter' || e.key === ' ') toggleControls()
+              }}
+              onClick={() => toggleControls()}
+            >
+              <div className="Key__icon Tiles__key-sc-icon"><i className="fa-solid fa-eye"></i></div>
+            </div>
+          </Tooltip>
         </div>
       </div>
-    </div>
+
+      <div className={`Tiles__footer ${tilesToggled && !hideControls ? 'Tiles__footer--show' : ''} ${slowLoad ? 'Tiles__footer--slow-load' : ''}`}>
+        <div className="Tiles__footer-controls">
+          <Tooltip direction="top" delay={0} content="Previous quote">
+            <div
+              tabIndex={0}
+              role="button"
+              className="Key Tiles__key-sc Tiles__key-sc--previous"
+              onKeyDown={(e) => {
+                if (e.key === 'Enter' || e.key === ' ') handleQuotePrevious()
+              }}
+              onClick={() => handleQuotePrevious()}
+            >
+              <div className="Key__icon Tiles__key-sc-icon"><i className="fa-solid fa-caret-left" /></div>
+            </div>
+          </Tooltip>
+
+          <Tooltip direction="top" delay={0} content="Next quote">
+            <div
+              tabIndex={0}
+              role="button"
+              className="Key Tiles__key-sc Tiles__key-sc--next"
+              onKeyDown={(e) => {
+                if (e.key === 'Enter' || e.key === ' ') handleQuoteNext()
+              }}
+              onClick={() => handleQuoteNext()}
+            >
+              <div className="Key__icon Tiles__key-sc-icon"><i className="fa-solid fa-caret-right" /></div>
+            </div>
+          </Tooltip>
+
+          <Tooltip direction="top" delay={0} content="Hide controls">
+            <div
+              tabIndex={0}
+              role="button"
+              className="Key Tiles__key-sc Tiles__key-sc--hide"
+              onKeyDown={(e) => {
+                if (e.key === 'Enter' || e.key === ' ') toggleControls()
+              }}
+              onClick={() => toggleControls()}
+            >
+              <div className="Key__icon Tiles__key-sc-icon"><i className="fa-solid fa-eye-slash"></i></div>
+            </div>
+          </Tooltip>
+
+          <Tooltip direction="top" delay={0} content="Toggle background">
+            <div
+              tabIndex={0}
+              role="button"
+              className="Key Tiles__key-sc Tiles__key-sc--background"
+              onKeyDown={(e) => {
+                if (e.key === 'Enter' || e.key === ' ') toggleBackground()
+              }}
+              onClick={() => toggleBackground()}
+            >
+              <div className="Key__icon Tiles__key-sc-icon"><i className="fa-solid fa-images"></i></div>
+            </div>
+          </Tooltip>
+
+          <Tooltip direction="top" delay={0} content="Toggle fullscreen">
+            <div
+              tabIndex={0}
+              role="button"
+              className="Key Tiles__key-sc Tiles__key-sc--fullscreen"
+              onKeyDown={(e) => {
+                if (e.key === 'Enter' || e.key === ' ') toggleFullScreen()
+              }}
+              onClick={() => toggleFullScreen()}
+            >
+              <div className="Key__icon Tiles__key-sc-icon"><i className="fa-solid fa-expand"></i></div>
+            </div>
+          </Tooltip>
+        </div>
+      </div>
+    </>
   );
 }
