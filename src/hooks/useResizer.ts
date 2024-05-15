f2ac07d5cb6cbdf32866e1be0d25a06a2ffbb874
Thendup Tsering <thendupcodes@gmail.com>
2024-05-15 10:35:10 -0400

    Formatting ./src/hooks/useResizer.ts

--- src/hooks/useResizer.ts
+++ src/hooks/useResizer.ts
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-export const useResizer = () : { windowWidth: number, windowHeight: number } => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+export const useResizer = (): { windowWidth: number; windowHeight: number } => {
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	const [windowHeight, setWindowHeight] = useState(window.innerHeight);
 
-  useEffect(() => {
-    const handleWindowResize = () => {
-      setWindowWidth(window.innerWidth);
-      setWindowHeight(window.innerHeight);
-    }
+	useEffect(() => {
+		const handleWindowResize = () => {
+			setWindowWidth(window.innerWidth);
+			setWindowHeight(window.innerHeight);
+		};
 
-    window.addEventListener('resize', handleWindowResize);
+		window.addEventListener('resize', handleWindowResize);
 
-    return (() => {
-      window.removeEventListener('resize', handleWindowResize);
-    })
-  }, [])
+		return () => {
+			window.removeEventListener('resize', handleWindowResize);
+		};
+	}, []);
 
-  return { windowWidth, windowHeight };
-}
+	return { windowWidth, windowHeight };
+};
